d125aeb48e358f0ef5d676fdf505b11d714f876b
BERRAMOU2017 <azz-eddine.BERRAMOU@edu.uca.ac.ma>
2017-03-27 15:02:11 +0100

    adding files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,31 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { NewQuoteComponent } from './new-quote/new-quote.component';
+import {routing} from "./quotes/app.routing";
+import {QuoteService} from "./quote.service";
+import { SingupComponent } from './singup/singup.component';
+import { SinginComponent } from './singin/singin.component';
+import {AuthService} from "./auth.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuotesComponent,
+    NewQuoteComponent,
+    SingupComponent,
+    SinginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [QuoteService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
